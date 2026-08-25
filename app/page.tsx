4299495daca1f147628ca67a714c78d0eb6c2e84
mesavic3-png <mesavic3@gmail.com>
2026-08-25 19:20:09 -0400

    fix: remove publish listing CTA from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,10 @@ import {FeaturedProperties} from "@/components/FeaturedProperties";
 import {Footer} from "@/components/Footer";
 import {Hero} from "@/components/Hero";
 import {Navbar} from "@/components/Navbar";
-import {OwnerCTA} from "@/components/OwnerCTA";
 import {PropertyAiAssistant} from "@/components/PropertyAiAssistant";
 
 export const dynamic = "force-dynamic";
 
 export default function Home(){
-  return <main><Navbar/><Hero/><PropertyAiAssistant compact/><AdSlot/><FeaturedProperties/><OwnerCTA/><Footer/></main>;
+  return <main><Navbar/><Hero/><PropertyAiAssistant compact/><AdSlot/><FeaturedProperties/><Footer/></main>;
 }
